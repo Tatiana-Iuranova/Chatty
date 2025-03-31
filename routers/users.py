6d85e4cdcfb1deb10929513aef7ba58cfab6f7e8
aysel-kh <khankishiyevaysel@gmail.com>
2025-03-31 21:39:28 +0200

    Добавлена новая схема UserResponse без пароля.
    Изменён response_model на UserResponse, чтобы избежать ResponseValidationError.
    
    Исправлена передача email (str(user_in.email))

--- routers/users.py
+++ routers/users.py
@@ -9,7 +9,7 @@ from utils.security import get_password_hash
 router = APIRouter()
 
 
-@router.post("/register", response_model=schemas.UserCreate)
+@router.post("/register", response_model=schemas.UserResponse)
 async def create_user(user_in: schemas.UserCreate, db: AsyncSession = Depends(get_db)):
     # Проверяем, нет ли такого пользователя
     existing_user = await db.execute(select(models.User).where(models.User.username == user_in.username))
@@ -21,6 +21,7 @@ async def create_user(user_in: schemas.UserCreate, db: AsyncSession = Depends(ge
     # Создаем пользователя
     user = models.User(
         username=user_in.username,
+        email=str(user_in.email),
         hashed_password=get_password_hash(user_in.password)
     )
 
